2e78ea98eae89ff4e839127e9a52d83ea56df2b4
Sergei <r4w6346@mail.ru>
2018-12-09 04:15:56 +0600

    Good_Update
    added walking system and animation

--- LaboratoryNew/Assets/Controller.cs
+++ LaboratoryNew/Assets/Controller.cs
@@ -3,35 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour {
-public GameObject player;
-public int speedRotation = 3;
-public int speed = 5;
-public int jumpSpeed = 50;
-public Animation animation;
+   float moveSpeed = 6f;   // Player's speed when walking. 
+   Animator animator;
+   Vector3 moveDirection; 
+   Rigidbody rb; 
 
-void Start () { 
-  player = (GameObject)this.gameObject; 
+   void Start() 
+   { 
+    rb = GetComponent<Rigidbody>(); 
+	animator = GetComponent<Animator>();
+   }
+  void Update()
+  {
+	 if (Input.GetKey(KeyCode.W))
+     {		 
+      float hAxis = Input.GetAxis("Horizontal"); 
+      float vAxis = Input.GetAxis("Vertical"); 
+
+      Vector3 movement = new Vector3(hAxis, 0f, vAxis); 
+      rb.position += movement * moveSpeed * Time.deltaTime; 
+	  animator.SetBool("Walk", true);
+	 }
   }
-  void Update(){
-if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) 
-  { 
-  player.transform.position += player.transform.forward * speed * Time.deltaTime; 
-  } 
-if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) 
-  { 
-  player.transform.position -= player.transform.forward * speed * Time.deltaTime; 
-  } 
-if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) 
-  { 
-  player.transform.Rotate(Vector3.down * speedRotation); 
-  } 
-  if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) 
-  { 
-  player.transform.Rotate(Vector3.up * speedRotation); 
-  } 
-if (Input.GetKeyDown(KeyCode.Space)) 
-  { 
-  player.transform.position += player.transform.up * jumpSpeed * Time.deltaTime; 
-  } 
- }
 }
