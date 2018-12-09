d44a41538ee51572b7de87848fd285dc5f2e4da5
Sergei <r4w6346@mail.ru>
2018-12-09 15:13:12 +0600

    Walk_Update
    Yeeas

---
+++ LaboratoryNew/Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    public float moveSpeed = 6f;   // Player's speed when walking. 
+    Animator animator;
+    Vector3 moveDirection; 
+    Rigidbody rb; 
+
+    void Start() 
+    { 
+        rb = GetComponent<Rigidbody>(); 
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.W)) && Input.GetKey(KeyCode.LeftShift))
+        {
+            float hAxis = Input.GetAxis("Horizontal");
+            float vAxis = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis);
+            rb.position += movement * (moveSpeed + 5) * Time.deltaTime;
+            animator.SetBool("Walking", true);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {		 
+            float hAxis = Input.GetAxis("Horizontal"); 
+            float vAxis = Input.GetAxis("Vertical"); 
+
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis); 
+            rb.position += movement * moveSpeed * Time.deltaTime; 
+            animator.SetBool("Walking", true);
+		}
+    }
+}
