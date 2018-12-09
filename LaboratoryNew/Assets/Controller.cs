15fb5d299bd03acae47504df073cb118a6deea27
Andrew89RUS <42917657+Andrew89RUS@users.noreply.github.com>
2018-12-09 12:46:30 +0500

    Update_Control_1

--- LaboratoryNew/Assets/Controller.cs
+++ LaboratoryNew/Assets/Controller.cs
@@ -1,28 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Controller : MonoBehaviour {
-   float moveSpeed = 6f;   // Player's speed when walking. 
-   Animator animator;
-   Vector3 moveDirection; 
-   Rigidbody rb; 
+    public float moveSpeed = 6f;   // Player's speed when walking. 
+    Animator animator;
+    Vector3 moveDirection; 
+    Rigidbody rb; 
 
-   void Start() 
-   { 
-    rb = GetComponent<Rigidbody>(); 
-	animator = GetComponent<Animator>();
-   }
-  void Update()
-  {
-	 if (Input.GetKey(KeyCode.W))
-     {		 
-      float hAxis = Input.GetAxis("Horizontal"); 
-      float vAxis = Input.GetAxis("Vertical"); 
+    void Start() 
+    { 
+        rb = GetComponent<Rigidbody>(); 
+        animator = GetComponent<Animator>();
+    }
 
-      Vector3 movement = new Vector3(hAxis, 0f, vAxis); 
-      rb.position += movement * moveSpeed * Time.deltaTime; 
-	  animator.SetBool("Walk", true);
-	 }
-  }
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)
+            || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow)) && Input.GetKey(KeyCode.LeftShift))
+        {
+            float hAxis = Input.GetAxis("Horizontal");
+            float vAxis = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis);
+            rb.position += movement * (moveSpeed + 5) * Time.deltaTime;
+            animator.SetBool("Walk", true);
+        }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) 
+            || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.UpArrow))
+        {		 
+            float hAxis = Input.GetAxis("Horizontal"); 
+            float vAxis = Input.GetAxis("Vertical"); 
+
+            Vector3 movement = new Vector3(hAxis, 0f, vAxis); 
+            rb.position += movement * moveSpeed * Time.deltaTime; 
+            animator.SetBool("Walk", true);
+	    }
+    }
 }
