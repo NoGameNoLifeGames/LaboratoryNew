b1afcf61be2a4be7121ddb11ab717720bb206ca7
Sergei <r4w6346@mail.ru>
2018-12-09 18:09:21 +0600

    Global_update_1
    1

---
+++ LaboratoryNew/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/AnimationPlay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour {
+	 Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+		{
+	       animator.SetBool("Walk", true);
+		}
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+	        animator.SetBool("Walk", false);
+		}
+	}
+}
